0bd2b2e9e470753b1276e704d9e128c32bb2004e
blackbhc <blackbhc@icloud.com>
2023-10-26 19:23:53 +0800

    finish the trim and split function

--- src/parameter/ini-parser.cpp
+++ src/parameter/ini-parser.cpp
@@ -1,14 +1,25 @@
 #include "ini-parser.h"
 #include "../tools/prompt.h"
+#include <algorithm>
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
 
 namespace galotfa {
+
+const std::string ini_parser::blank          = " \t\n\r\f\v";
+const std::string ini_parser::comment_prefix = "#;";
+const std::string ini_parser::section_prefix = "[";
+const std::string ini_parser::section_suffix = "]";
+const std::string ini_parser::key_value_sep  = "=";
+const std::string ini_parser::value_sep      = " \t,-:+&";
+
 ini_parser::ini_parser( const char* file_name )
 {
+#ifndef debug_parameter  // no initialization in debug mode
     this->filename = file_name;
     this->read( this->filename.c_str() );
+#endif
 }
 
 void ini_parser::read( const char* filename )
@@ -17,40 +28,39 @@ void ini_parser::read( const char* filename )
     stat( filename, st );
     this->check_filesize( st->st_size );
 
-    // // read the file into one buffer
-    // FILE* fp = fopen( filename, "rb" );
-    // if ( fp == NULL )
-    //     ERROR( "The file %s does not exist!\n", filename );
-    //
-    // char* buffer = new char[ st->st_size ];
-    // fread( buffer, st->st_size, 1, fp );
-    // // close the file
-    // fclose( fp );
-    //
-    // char* token = strsep( &buffer, "\n" );
-    // while ( token != NULL )
-    // {
-    //     // While there are tokens in "string"
-    //     if ( *token == '\0' )
-    //         ;
-    //     else
-    //     {
-    //         std::string str( token );
-    //         str.erase( 0, str.find_first_not_of( " " ) );
-    //         if ( str[ 0 ] != '#' )
-    //             println( " %s\n", str.c_str() );
-    //     }
-    //     // Get next token:
-    //     token = strsep( &buffer, "\n" );
-    // }
-    //
-    // delete[] buffer;
+    // read the file into one buffer
+    FILE* fp = fopen( filename, "rb" );
+    if ( fp == NULL )
+    {
+        delete st;
+        st = nullptr;
+        ERROR( "The file %s does not exist!\n", filename );
+    }
+    char buffer[ st->st_size + 1 ];
+    buffer[ st->st_size ] = '\0';
+    char* p_buffer        = buffer;
+    fread( p_buffer, st->st_size, 1, fp );
+
+    // parse the buffer
+    char* token = strsep( &p_buffer, "\n" );  // get the first line
+    while ( token != NULL )
+    {
+        if ( *token == '\0' )
+        {
+        }
+        else
+        {
+            this->lines.push_back( this->line_parser( token ) );
+        }
+        token = strsep( &p_buffer, "\n" );
+    }
+
+    fclose( fp );
     delete st;
-    // buffer = nullptr;
     st = nullptr;
 }
 
-void ini_parser::check_filesize( long int size )
+void ini_parser::check_filesize( long int size ) const
 {
     if ( size == 0 )
     {
@@ -62,13 +72,113 @@ void ini_parser::check_filesize( long int size )
     }
     else if ( size >= 1024 * 1024 * 1024 )
     {
-        ERROR( " The ini file is too big: size = %ld MB\nCheck whether the file is correct!",
+        ERROR( " The ini file is too big: size = %ld MB\nPlease check whether the your file is "
+               "correct!",
                size / 1024 / 1024 );
     }
 }
 
+ini_parser::Line ini_parser::line_parser( const char* str ) const
+{
+    ini_parser::Line line;
+    std::string      raw_content = str;
+    raw_content                  = this->trim( raw_content );
+
+    // check whether it is a section
+    if ( raw_content.size() == 0 )  // empty line
+    {
+        line.type = LineType::white;
+        return line;
+    }
+    else if ( raw_content.find_first_of( section_prefix ) == 0 )
+    // section header
+    {
+        if ( raw_content.find_first_of( section_suffix ) == raw_content.size() - 1 )
+        {
+            line.type = LineType::section;
+            raw_content.erase( 0, raw_content.find_first_of( section_prefix ) + 1 );
+            raw_content.erase( raw_content.find_first_of( section_suffix ) );
+            line.content = raw_content;
+            return line;
+        }
+        else
+        {
+            ERROR( "Get an invalid section header in ini file:\n%s", str );
+        }
+    }
+    else if ( raw_content.find_first_of( key_value_sep ) != std::string::npos )
+    // key-value pair
+    {
+        line.type = LineType::key_value;
+        std::string key =
+            this->trim( raw_content.substr( 0, raw_content.find_first_of( key_value_sep ) ) );
+        std::string val =
+            this->trim( raw_content.substr( raw_content.find_first_of( key_value_sep ) + 1 ) );
+        line.content = key + "=" + val;
+        // get the value type
+        std::transform( val.begin(), val.end(), val.begin(), ::tolower );
+        if ( val == "true" || val == "yes" || val == "1" || val == "enable" || val == "on"
+             || val == "false" || val == "no" || val == "0" || val == "disable" || val == "off" )
+        {
+            line.type       = LineType::key_value;
+            line.value_type = ValueType::boolean;
+            return line;
+        }
+        else
+        {
+            if ( val.find_first_of( value_sep ) == std::string::npos )
+            {
+                line.type       = LineType::key_value;
+                line.value_type = ValueType::string;
+                return line;
+            }
+            else
+            {
+                line.type       = LineType::key_value;
+                line.value_type = ValueType::number;
+                return line;
+            }
+        }
+    }
+    else
+    {
+        ERROR( "Get an invalid line in ini file:\n%s", str );
+    }
+}
+
+inline std::string ini_parser::trim( std::string str ) const
+{
+    str.erase( 0, str.find_first_not_of( blank ) );
+    str.erase( str.find_last_not_of( blank ) + 1 );
+    // remove the possible comments
+    if ( str.find_first_of( comment_prefix ) != std::string::npos )
+    {
+        str.erase( str.find_first_of( comment_prefix ) );
+    }
+    return str;
+}
+
+inline std::vector< std::string > ini_parser::split( std::string str ) const
+{
+    std::vector< std::string > vals;
+    str = this->trim( str );
+    while ( true )
+    {
+        auto pos = str.find_first_of( value_sep );
+        vals.push_back( str.substr( 0, pos ) );
+        str.erase( 0, pos );
+        str.erase( 0, str.find_first_not_of( value_sep ) );
+        if ( str.find_first_of( value_sep ) == std::string::npos )
+        {
+            vals.push_back( str );
+            break;
+        }
+    }
+    return vals;
+}
+
 #ifdef debug_parameter
-int ini_parser::test_check_size_works()
+int ini_parser::test_checksize()
 {
     println( "Testing ini_parser::check_filesize() ..." );
     try
@@ -123,6 +233,88 @@ int ini_parser::test_check_size_works()
     catch ( const std::exception& e )
     {
         println( "It failed unexpectedly, error message: %s.", e.what() );
+        return 1;
+    }
+}
+
+int ini_parser::test_lineparser( void )
+{
+    println( "Testing ini_parser::line_parser(const char* str) ..." );
+
+    /* char empty_line[]      = "";
+    char blank_line[]      = "\t  \n";
+    char comment_line[]    = "# this is a comment";
+    char section_line[]    = "[This is a section]";
+    char key_int_line[]    = "key_int = 1";
+    char key_double_line[] = "key_char = 1.0";
+    char key_bool_line[]   = "key_bool = true";
+    char key_str_line[]    = "key_str = this is a string"; */
+
+    return 0;
+}
+
+int ini_parser::test_trim( void )
+{
+    println( "Testing ini_parser::trim(std::string str) ..." );
+
+    char str1[] = "  \t  \n  \r  \f  \v  OK";
+    char str2[] = "  \t  \n  \r  \f  \v  # test the comment";
+    char str3[] = "# This is a comment \n\n";
+    char str4[] = "  \t  \n  \r  \f  \vstr before comment# asdqw";
+    char str5[] = " enclosed by space ";
+    char str6[] = "1:::-+2;3-4";
+
+    bool res1 = trim( str1 ) == "OK";
+    bool res2 = trim( str2 ) == "";
+    bool res3 = trim( str3 ) == "";
+    bool res4 = trim( str4 ) == "str before comment";
+    bool res5 = trim( str5 ) == "enclosed by space";
+    bool res6 = trim( str6 ) == "1:::-+2";
+
+    bool success = res1 && res2 && res3 && res4 && res5 && res6;
+
+    if ( success )
+    {
+        println( "It passed the test." );
+        return 0;
+    }
+    else
+    {
+        WARN( "It failed the test." );
+        return 1;
+    }
+}
+
+#include <assert.h>
+int ini_parser::test_split( void )
+{
+    println( "Testing ini_parser::split(std::string str) ..." );
+
+    std::string str1 = " 1,2,3 4";
+    std::string str2 = "1 2\t 3 \t\t4";
+    std::string str3 = "1+2:3:4";
+    std::string str4 = "1:::-+2\t3-4";
+    std::string str5 = "1.012+string+3&4";
+
+    std::vector< std::string > target1 = { "1", "2", "3", "4" };
+    std::vector< std::string > target2 = { "1.012", "string", "3", "4" };
+
+    auto res1 = split( trim( str1 ) );
+    auto res2 = split( trim( str2 ) );
+    auto res3 = split( trim( str3 ) );
+    auto res4 = split( trim( str4 ) );
+    auto res5 = split( trim( str5 ) );
+
+    bool success = ( res1 == target1 ) && ( res2 == target1 ) && ( res3 == target1 )
+                   && ( res4 == target1 ) && ( res5 == target2 );
+    if ( success )
+    {
+        println( "It passed the test." );
+        return 0;
+    }
+    else
+    {
+        WARN( "It failed the test." );
         return 1;
     }
 }
--- src/parameter/ini-parser.h
+++ src/parameter/ini-parser.h
@@ -4,32 +4,49 @@
 #ifndef __INI_PARSER_H__
 #define __INI_PARSER_H__
 #include <string>
+#include <vector>
 namespace galotfa {
 class ini_parser
 {
-    // public methods
-public:
+private:  // private members
+    enum class ValueType { number, string, boolean, none };
+    enum class LineType { section, key_value, white };
+    struct Line
+    {
+        std::string content    = "";
+        LineType    type       = LineType::white;
+        ValueType   value_type = ValueType::none;
+    };
+    // define the delimiters
+    static const std::string blank;
+    static const std::string comment_prefix;
+    static const std::string section_prefix;
+    static const std::string section_suffix;
+    static const std::string key_value_sep;
+    static const std::string value_sep;
+    std::vector< Line >      lines;
+
+public:  // public members
+    std::string filename;
+
+public:  // public methods
     ini_parser( const char* filename );
     ~ini_parser() = default;
 #ifdef debug_parameter
-    int test_check_size_works();
+    int test_checksize();
+    int test_trim();
+    int test_split();
+    int test_lineparser();
 #endif
-    // private methods
-private:
-    void read( const char* filename );
-    void check_filesize( long int size );
 
-    // public members
-public:
-    std::string filename;
-    // private members
+private:  // private methods
+    void                              read( const char* filename );
+    Line                              line_parser( const char* str ) const;
+    void                              check_filesize( long int size ) const;
+    inline std::string                trim( std::string str ) const;
+    inline std::vector< std::string > split( std::string str ) const;
 };
 
 }  // namespace galotfa
-
-namespace ini {
-enum class Valuetype { character, number, string, boolean, none };
-enum class Linetype { comment, section, key_value, white };
-}  // namespace ini
 
 #endif
