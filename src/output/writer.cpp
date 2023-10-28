efe09c4405d760be96c233ddd1fcde790d89867d
blackbhc <blackbhc@icloud.com>
2023-10-28 22:48:45 +0800

    backup the writer class dev codes

--- src/output/writer.cpp
+++ src/output/writer.cpp
@@ -1,39 +1,80 @@
 #ifndef __GALOTFA_WRITER_CPP__
 #define __GALOTFA_WRITER_CPP__
 #include "writer.h"
+#include "../tools/prompt.h"
+#include "../tools/string.h"
+#ifdef DO_UNIT_TEST
+#include "../tools/string.cpp"
+#endif
 #include <hdf5.h>
-#include <stdio.h>
 #include <unistd.h>
 
 namespace galotfa {
-hid_t create_h5( std::string path_to_file )
+writer::writer( std::string path_to_file )
 {
-    hid_t file_id = -1;
-    if ( access( path_to_file.c_str(), F_OK ) == 0 )
-    // if file exists
-    {
-        // add suffix to file name to avoid overwriting
-        int i = 1;
-        while ( access( ( path_to_file + "-" + std::to_string( i ) ).c_str(), F_OK ) == 0 )
-        {
-            ++i;
-        }
-        path_to_file += "-" + std::to_string( i );
-    }
-    file_id = H5Fcreate( path_to_file.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT );
-    return file_id;
+#ifndef debug_output  // if not in debug mode: create file
+    this->create_file( path_to_file );
+#else  // if in debug mode: do nothing
+    ( void )path_to_file;
+#endif
 }
-}  // namespace galotfa
+
+writer::~writer( void )
+{
+#ifndef debug_output  // if not in debug mode: close file
+    for ( auto& property_handle : this->property_handles )
+        H5Pclose( property_handle );
+    for ( auto& dataset_handle : this->dataset_handles )
+        H5Dclose( dataset_handle );
+    for ( auto& dataspace_handle : this->dataspace_handles )
+        H5Sclose( dataspace_handle );
+    for ( auto& group_handle : this->group_handles )
+        H5Gclose( group_handle );
+    H5Fclose( this->file_id );
+#endif
+}
+
+int writer::create_file( std::string path_to_file )
+{
+    try
+    {
+        if ( access( path_to_file.c_str(), F_OK ) == 0 )
+        // if file exists
+        {
+            // add suffix to file name to avoid overwriting
+            int i = 1;
+            while ( access( ( path_to_file + "-" + std::to_string( i ) ).c_str(), F_OK ) == 0 )
+            {
+                ++i;
+            }
+            path_to_file += "-" + std::to_string( i );
+        }
+        this->file_id = H5Fcreate( path_to_file.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT );
+        return 0;
+    }
+    catch ( ... )
+    {
+        WARN( "Failed to create file: %s", path_to_file.c_str() );
+        return 1;
+    }
+}
+
+
+int writer::create_group( std::string name )
+{
+    return 0;
+}
+
 
 #ifdef debug_output
-namespace unit_test {
-int test_create_h5()
+int writer::test_create_file()
 {
     // test it can create a file
-    std::string testfile = "test1.h5";
-    hid_t       file_id  = galotfa::create_h5( testfile );
+    std::string testfile    = "test1.h5";
+    int         return_code = this->create_file( testfile );
+    hid_t       file_id     = this->file_id;
     H5Fclose( file_id );
-    bool create_success = access( testfile.c_str(), F_OK ) == 0;
+    bool create_success = access( testfile.c_str(), F_OK ) == 0 && return_code == 0;
     if ( create_success )
         remove( testfile.c_str() );
 
@@ -41,9 +82,11 @@ int test_create_h5()
     std::string testfile2 = "test2.h5";
     FILE*       fp        = fopen( testfile2.c_str(), "w" );
     fclose( fp );
-    file_id = galotfa::create_h5( testfile2 );
+    return_code = this->create_file( testfile2 );
+    file_id     = this->file_id;
     H5Fclose( file_id );
-    bool create_another_success = access( ( testfile2 + "-1" ).c_str(), F_OK ) == 0;
+    bool create_another_success =
+        access( ( testfile2 + "-1" ).c_str(), F_OK ) == 0 && return_code == 0;
     if ( create_another_success )
         remove( ( testfile2 + "-1" ).c_str() );
     remove( testfile2.c_str() );
@@ -52,6 +95,6 @@ int test_create_h5()
 
     CHECK_RETURN( success );
 }
-}  // namespace unit_test
 #endif
+}  // namespace galotfa
 #endif
--- src/output/writer.h
+++ src/output/writer.h
@@ -1,15 +1,44 @@
 #ifndef __GALOTFA_WRITER_H__
 #define __GALOTFA_WRITER_H__
 #include <hdf5.h>
+#include <list>
 #include <string>
+#include <unordered_map>
 namespace galotfa {
-hid_t create_h5( std::string path_to_file );
-}  // namespace galotfa
+enum class H5DataType { scalar, vector, matrix, tensor };
+
+class writer
+{
+    // public members
+public:
+    // private members
+private:
+    // list of handles: to be closed in destructor, for group, dataset, dataspace, property
+    std::list< hid_t >                       group_handles;
+    std::list< hid_t >                       dataset_handles;
+    std::list< hid_t >                       dataspace_handles;
+    std::list< hid_t >                       property_handles;
+    hid_t                                    file_id = 0;
+    std::unordered_map< std::string, hid_t > groups;
+    std::unordered_map< std::string, hid_t > datasets;
+
+
+
+    // public methods
+public:
+    writer( std::string path_to_file );
+    ~writer( void );
+    int   create_file( std::string path_to_file );
+    int   create_group( std::string name );
+    int   create_dataset( hid_t parent_id, std::string name );
+    hid_t operator[]( std::string name );  // return a hdf5 handle from the map
+#ifdef debug_output
+    int test_create_file( void );
 #endif
 
+    // private methods
+private:
+};
 
-#ifdef debug_output
-namespace unit_test {
-int test_create_h5( void );
-}  // namespace unit_test
+}  // namespace galotfa
 #endif
